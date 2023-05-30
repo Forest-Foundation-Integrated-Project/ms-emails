3a9a318e0bd5060c8bb463c018d87bed1b9dabc6
Alexandre Gandolfo <alegandolfo10@gmail.com>
2023-05-30 19:15:07 -0300

    chore: add send mail service

---
+++ src/4-framework/service/mailService.ts
@@ -0,0 +1,45 @@
+import { injectable } from "inversify";
+import { IMailEntity } from "../../1-domain/entities/mailEntity";
+import { IMailService } from "../../2-business/services/iMailService";
+import { SES } from 'aws-sdk';
+
+@injectable()
+export class MailService implements IMailService {
+  private readonly ses: SES
+
+  constructor() {
+    this.ses = new SES()
+  }
+
+  async send(mailEntity: IMailEntity): Promise<boolean> {
+
+    const params = {
+      Source: 'alumnus.testes@gmail.com',
+      Destination: {
+        ToAddresses: [mailEntity.targetEmail]
+      },
+      Message: {
+        Body: {
+          Text: {
+            Data: 'Tesde de Email'
+          }
+        },
+        Subject: {
+          Data: 'Email de Teste'
+        }
+      }
+    }
+
+    try {
+      const result = await this.ses.sendEmail(params).promise()
+      console.log(result)
+
+      return true
+
+    } catch (error) {
+      console.log(error)
+
+      return false
+    }
+  }
+}
