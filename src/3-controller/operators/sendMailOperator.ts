0c93d0ad38699c81c94e3b40a0afefd8d7ebf144
Alexandre Gandolfo <alegandolfo10@gmail.com>
2023-05-30 19:14:31 -0300

    chore: add send mail operator

---
+++ src/3-controller/operators/sendMailOperator.ts
@@ -0,0 +1,23 @@
+import { injectable, inject } from 'inversify'
+
+import { left, right } from '../../4-framework/shared/either'
+import { AbstractOperator } from './abstractOperator'
+import { InputSendMail, OutputSendMail } from '../serializers/inputSendMail'
+import { SendMailUseCase } from '../../2-business/useCases/sendMailUseCase'
+
+@injectable()
+export class SendMailOperator extends AbstractOperator<InputSendMail, OutputSendMail> {
+  public constructor(@inject(SendMailUseCase) private sendMailUseCase: SendMailUseCase) {
+    super()
+  }
+
+  protected async run(input: InputSendMail): Promise<OutputSendMail> {
+    const result = await this.sendMailUseCase.exec(input)
+
+    if (result.isLeft()) {
+      return left(result.value)
+    }
+
+    return right(result.value)
+  }
+}
