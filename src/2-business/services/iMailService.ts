31d92217cdd9fd45d37683203d2fda14e966f563
Alexandre Gandolfo <alegandolfo10@gmail.com>
2023-05-30 21:57:01 -0300

    refactor: change product entity to mail entity

--- src/2-business/services/iMailService.ts
+++ src/2-business/services/iMailService.ts
@@ -3,5 +3,5 @@ import { IMailEntity } from "../../1-domain/entities/mailEntity"
 export const IMailServiceToken = Symbol.for('IMailService')
 
 export interface IMailService {
-  send(productEntity: IMailEntity): Promise<boolean>
+  send(mailEntity: IMailEntity): Promise<boolean>
 }
